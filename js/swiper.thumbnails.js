ac422ad8ba25bf4a1bc8f9639f0076cdb677f1cc
Gijs Rogé <rogegijs@gmail.com>
2016-07-29 15:23:31 +0200

    Added swiperThumbs to window object and add jQuery reference wrapper to prevent collision with other frameworks

--- js/swiper.thumbnails.js
+++ js/swiper.thumbnails.js
@@ -3,96 +3,99 @@
  * @param swiper (pass swiper element)
  * @param settings (pass custom options)
  */
-var swiperThumbs = function (swiper, settings) {
-
-  /**
-   * Loop over swiper instances
-   */
-  $(swiper).each(function () {
-
-    var _this = this;
+(function ($, window) {
+  window.swiperThumbs = function (swiper, settings) {
 
     /**
-     * Default settings
+     * Loop over swiper instances
      */
-    var options = {
-      element: 'swiper-thumbnails',
-      activeClass: 'is-active'
-    }
+    $(swiper).each(function () {
 
-    /**
-     * Merge user settings and default settings
-     */
-    $.extend(options, settings);
+      var _this = this;
 
-    /**
-     * Helper vars
-     */
-    var element = $('.' + options.element);
-
-    /**
-     * Get real activeIndex
-     * @returns {*}
-     */
-    var realIndex = function (index) {
-      // if index doesn't exist set index to activeIndex of swiper
-      if (index === undefined) index = _this.activeIndex;
-
-      // Check if swiper instance has loop before getting real index
-      if(_this.params.loop){
-        return parseInt(_this.slides.eq(index).attr('data-swiper-slide-index'));
-      }else{
-        return index;
+      /**
+       * Default settings
+       */
+      var options = {
+        element: 'swiper-thumbnails',
+        activeClass: 'is-active'
       }
-    }
 
-    var app = {
+      /**
+       * Merge user settings and default settings
+       */
+      $.extend(options, settings);
 
-      init: function () {
-        app.bindUIevents();
-        app.updateActiveClasses(realIndex(_this.activeIndex));
-      },
+      /**
+       * Helper vars
+       */
+      var element = $('.' + options.element);
 
-      bindUIevents: function () {
-        /**
-         * Bind click events to thumbs
-         */
-        element.children().each(function () {
-          $(this).on('click', function () {
+      /**
+       * Get real activeIndex
+       * @returns {*}
+       */
+      var realIndex = function (index) {
+        // if index doesn't exist set index to activeIndex of swiper
+        if (index === undefined) index = _this.activeIndex;
 
-            // Get clicked index
-            var index = parseInt($(this).index());
+        // Check if swiper instance has loop before getting real index
+        if (_this.params.loop) {
+          return parseInt(_this.slides.eq(index).attr('data-swiper-slide-index'));
+        } else {
+          return index;
+        }
+      }
 
-            // Get difference between item clicked and current real active index.
-            var difference = (index - realIndex());
+      var app = {
 
-            // Move to slide that makes sense for the user by
-            // checking what the current active slide is and adding the difference
-            // this makes sure the swiper moves to a natural direction the user expects.
-            app.moveToSlide(_this.activeIndex + difference);
+        init: function () {
+          app.bindUIevents();
+          app.updateActiveClasses(realIndex(_this.activeIndex));
+        },
+
+        bindUIevents: function () {
+          /**
+           * Bind click events to thumbs
+           */
+          element.children().each(function () {
+            $(this).on('click', function () {
+
+              // Get clicked index
+              var index = parseInt($(this).index());
+
+              // Get difference between item clicked and current real active index.
+              var difference = (index - realIndex());
+
+              // Move to slide that makes sense for the user by
+              // checking what the current active slide is and adding the difference
+              // this makes sure the swiper moves to a natural direction the user expects.
+              app.moveToSlide(_this.activeIndex + difference);
+            })
           })
-        })
 
-        /**
-         * Update thumbs on slideChange
-         */
-        _this.on('slideChangeStart', function (swiper) {
-          app.updateActiveClasses(realIndex())
-        });
-      },
+          /**
+           * Update thumbs on slideChange
+           */
+          _this.on('slideChangeStart', function (swiper) {
+            app.updateActiveClasses(realIndex())
+          });
+        },
 
-      moveToSlide: function (index) {
-        _this.slideTo(index);
-      },
+        moveToSlide: function (index) {
+          _this.slideTo(index);
+        },
 
-      updateActiveClasses: function (index) {
-        element.children().removeClass(options.activeClass);
-        element.children().eq(index).addClass(options.activeClass);
+        updateActiveClasses: function (index) {
+          element.children().removeClass(options.activeClass);
+          element.children().eq(index).addClass(options.activeClass);
+        }
       }
-    }
 
-    app.init();
+      app.init();
 
-  });
+    });
 
-};
+  };
+
+}(jQuery, window));
